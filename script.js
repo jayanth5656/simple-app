4c7da16b5c810b34a7722d7b2422241ee1547a4b
jayanth5656 <133353573+jayanth5656@users.noreply.github.com>
2023-09-12 10:18:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+document.addEventListener('DOMContentLoaded', function () {
+    const myButton = document.getElementById('myButton');
+    
+    myButton.addEventListener('click', function () {
+        alert('Button clicked!');
+    });
+});
